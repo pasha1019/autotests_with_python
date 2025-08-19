84bc5dba9df1ef76c87b857988b647a0e6338151
Pavel <pashabugakov@yandex.ru>
2025-08-19 12:24:55 +0300

    Add first test

---
+++ base_test.py
@@ -0,0 +1,17 @@
+import time
+from selenium import webdriver
+
+
+with webdriver.Chrome() as browser:
+    browser.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    time.sleep(5)
+    username_field = browser.find_element('xpath', "//input[@name='username']")
+    pass_field = browser.find_element('xpath', "//input[@name='password']")
+    login_button = browser.find_element('xpath', "//button[@type='submit']")
+    username_field.clear()
+    pass_field.clear()
+    username_field.send_keys('Admin')
+    pass_field.send_keys('admin123')
+    time.sleep(5)
+    login_button.click()
+    time.sleep(5)
